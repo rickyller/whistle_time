1e463478deaf5bdbc9b581beaec84b1847691543
rickyller <richitork10@hotmail.com>
2024-11-05 16:44:57 -0600

    Revert "first commit"
    This reverts commit 7c9d450c7fe9b565c01a8a08f6c1dd060ad8bac6.

--- lib/screens_smartphone/añadir_partido.dart
+++ lib/screens_smartphone/añadir_partido.dart
@@ -11,24 +11,6 @@ class AddMatchScreen extends StatefulWidget {
 
 class _AddMatchScreenState extends State<AddMatchScreen> {
   String _selectedRole = 'Árbitro'; // Valor predeterminado
-
-  Map<String, TextEditingController> _roleControllers = {};
-  String _selectedLiga = 'Liga TDP'; // Valor predeterminado
-  String _selectedPeriodos = '2 mitades'; // Valor predeterminado
-  String _selectedPenaltis = "Sí"; // Valor predeterminado
-  String _selectedRegistrarGoleadores = "Sí"; // Valor predeterminado
-  String _selectedExpulsionesTemporales = "Sin expulsión temporal"; // Valor predeterminado
-  bool _rememberConfiguration = false; // Valor inicial
-  late TextEditingController _nombrePlantillaController;
-  late TextEditingController _numeroJugadoresController;
-  late TextEditingController _numeroSustitutosController;
-  late TextEditingController _duracionMitadController;
-  late TextEditingController _medioTiempoController;
-  late TextEditingController _lugarController;
-  late TextEditingController _equipoLocalController;
-  late TextEditingController _equipoVisitanteController;
-  late TextEditingController _equipoLocalAbreviadoController;
-  late TextEditingController _equipoVisitanteAbreviadoController;
   late DateTime _selectedDate; // Variable para la fecha seleccionada
   Color _localTeamColor = Colors.red; // Color predeterminado para equipo local
   Color _visitorTeamColor = Colors.teal; // Color predeterminado para equipo visitante
@@ -36,41 +18,8 @@ class _AddMatchScreenState extends State<AddMatchScreen> {
   @override
   void initState() {
     super.initState();
-
-    // Inicializamos controladores para cada rol
-    _roleControllers = {
-      'Árbitro': TextEditingController(),
-      'Asistente 1': TextEditingController(),
-      'Asistente 2': TextEditingController(),
-      'Cuarto Árbitro': TextEditingController(),
-      'Asesor': TextEditingController(),
-    };
-    // Controladores con valores iniciales porque son editables
-    _numeroJugadoresController = TextEditingController(text: "11");
-    _numeroSustitutosController = TextEditingController(text: "5");
-    _duracionMitadController = TextEditingController(text: "45");
-    _medioTiempoController = TextEditingController(text: "15");
-
-    // Los campos con hint no requieren inicialización con un valor
-    _nombrePlantillaController = TextEditingController(); // Hint visible cuando esté vacío
-    _lugarController = TextEditingController(); // Hint visible cuando esté vacío
-    _equipoLocalController = TextEditingController(); // Hint visible cuando esté vacío
-    _equipoVisitanteController = TextEditingController(); // Hint visible cuando esté vacío
-    _equipoLocalAbreviadoController = TextEditingController(); // Hint visible cuando esté vacío
-    _equipoVisitanteAbreviadoController = TextEditingController(); // Hint visible cuando esté vacío
-
     // Inicializar _selectedDate redondeando la hora actual
     _selectedDate = _roundToNearestHalfHour(DateTime.now());
-  }
-
-  @override
-  void dispose() {
-    _nombrePlantillaController.dispose();
-    _numeroJugadoresController.dispose();
-    _numeroSustitutosController.dispose();
-    _duracionMitadController.dispose();
-    _medioTiempoController.dispose();
-    super.dispose();
   }
 
   DateTime _roundToNearestHalfHour(DateTime dateTime) {
@@ -86,37 +35,19 @@ class _AddMatchScreenState extends State<AddMatchScreen> {
   }
 
 
-  Future<void> _selectDateAndTime(BuildContext context) async {
-    // Seleccionar la fecha
-    final DateTime? pickedDate = await showDatePicker(
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
       context: context,
       initialDate: _selectedDate,
       firstDate: DateTime(2000),
       lastDate: DateTime(2101),
     );
-
-    if (pickedDate != null && pickedDate != _selectedDate) {
-      // Seleccionar la hora
-      final TimeOfDay? pickedTime = await showTimePicker(
-        context: context,
-        initialTime: TimeOfDay.fromDateTime(_selectedDate), // Hora inicial basada en la fecha seleccionada
-      );
-
-      if (pickedTime != null) {
-        setState(() {
-          // Combina la fecha seleccionada con la hora seleccionada
-          _selectedDate = DateTime(
-            pickedDate.year,
-            pickedDate.month,
-            pickedDate.day,
-            pickedTime.hour,
-            pickedTime.minute,
-          );
-        });
-      }
+    if (picked != null && picked != _selectedDate) {
+      setState(() {
+        _selectedDate = _roundToNearestHalfHour(picked);
+      });
     }
   }
-
 
 
   void _openLocalColorPicker() {
@@ -211,21 +142,10 @@ class _AddMatchScreenState extends State<AddMatchScreen> {
                 "Datos del partido",
                 Column(
                   children: [
-                    _buildDropdownFieldLiga("Liga o competición", _selectedLiga),
-                    _buildInputField("Lugar", _lugarController, "Estadio 3 de Marzo"),
+                    _buildInputField("Competición", "Ej. Liga TDP"),
+                    _buildInputField("Lugar", "Ej. Estadio 3 de marzo"),
                     _buildDateField(),
-                    _buildDropdownField(
-                      label: "Rol del miembro arbitral",
-                      currentValue: _selectedRole,
-                      items: ['Árbitro', 'Árbitro asistente', 'Cuarto árbitro', 'Asesor'],
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          _selectedRole = newValue ?? 'Árbitro'; // Valor por defecto si newValue es null
-                        });
-                      },
-
-                    )
-
+                    _buildDropdownField("Rol del miembro arbitral", _selectedRole),
                   ],
                 ),
               ),
@@ -234,9 +154,8 @@ class _AddMatchScreenState extends State<AddMatchScreen> {
                 "Equipo Local",
                 Column(
                   children: [
-                    _buildInputField("Nombre del equipo", _equipoLocalController, "Tecos"),
-                    _buildInputField("Nombre abreviado", _equipoLocalAbreviadoController, "TEC"),
-
+                    _buildInputField("Nombre del equipo", "Ej. Tecos"),
+                    _buildInputField("Nombre abreviado", "Ej. TEC"),
                     _buildLocalTeamColorPickerIcon(), // Local team color picker
                   ],
                 ),
@@ -246,9 +165,8 @@ class _AddMatchScreenState extends State<AddMatchScreen> {
                 "Equipo Visitante",
                 Column(
                   children: [
-                    _buildInputField("Nombre del equipo", _equipoVisitanteController, "Nacional"),
-                    _buildInputField("Nombre abreviado", _equipoVisitanteAbreviadoController, "NAC"),
-
+                    _buildInputField("Nombre del equipo", "Ej. Nacional"),
+                    _buildInputField("Nombre abreviado", "Ej. NAC"),
                     _buildVisitorTeamColorPickerIcon(), // Visitor team color picker
                   ],
                 ),
@@ -283,12 +201,12 @@ class _AddMatchScreenState extends State<AddMatchScreen> {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: InkWell(
-        onTap: () => _selectDateAndTime(context), // Llama al selector de fecha
+        onTap: () => _selectDate(context), // Llama al selector de fecha
         child: InputDecorator(
           decoration: InputDecoration(
             labelText: "Fecha",
             filled: true,
-            fillColor: Colors.black, // Fondo gris oscuro para el campo de fecha
+            fillColor: Colors.grey[800], // Fondo gris oscuro para el campo de fecha
             border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(8),
               borderSide: BorderSide(color: Colors.grey),
@@ -303,12 +221,7 @@ class _AddMatchScreenState extends State<AddMatchScreen> {
     );
   }
 
-  Widget _buildDropdownField({
-    required String label,
-    required String currentValue,
-    required List<String> items,
-    required Function(String?) onChanged,
-  }) {
+  Widget _buildDropdownField(String label, String currentValue) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: DropdownButtonFormField<String>(
@@ -316,44 +229,14 @@ class _AddMatchScreenState extends State<AddMatchScreen> {
         decoration: InputDecoration(
           labelText: label,
           filled: true,
-          fillColor: AppColors.black, // Fondo gris oscuro
+          fillColor: AppColors.darkGrey, // Fondo gris oscuro
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(8),
             borderSide: BorderSide(color: AppColors.grey), // Borde gris claro
           ),
         ),
-        dropdownColor: AppColors.black, // Color del dropdown
-        items: items.map<DropdownMenuItem<String>>((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(
-              value,
-              style: TextStyle(color: AppColors.white), // Texto en blanco para las opciones
-            ),
-          );
-        }).toList(),
-        onChanged: onChanged, // Pasar la función onChanged que se adapta según el caso
-      ),
-    );
-  }
-
-
-  Widget _buildDropdownFieldLiga(String label, String currentValue) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: DropdownButtonFormField<String>(
-        value: currentValue,
-        decoration: InputDecoration(
-          labelText: label,
-          filled: true,
-          fillColor: AppColors.black, // Fondo gris oscuro
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
-            borderSide: BorderSide(color: AppColors.black), // Borde gris claro
-          ),
-        ),
         dropdownColor: AppColors.darkGrey, // Color del dropdown
-        items: <String>['Liga TDP', 'Amateur', 'Sub 15', 'Sub 17', 'Sub 19', 'Sub 23' ]
+        items: <String>['Árbitro', 'Árbitro asistente', 'Cuarto árbitro', 'Asesor']
             .map<DropdownMenuItem<String>>((String value) {
           return DropdownMenuItem<String>(
             value: value,
@@ -365,7 +248,7 @@ class _AddMatchScreenState extends State<AddMatchScreen> {
         }).toList(),
         onChanged: (String? newValue) {
           setState(() {
-            _selectedLiga = newValue!; // Actualiza el rol seleccionado
+            _selectedRole = newValue!; // Actualiza el rol seleccionado
           });
         },
       ),
@@ -399,24 +282,22 @@ class _AddMatchScreenState extends State<AddMatchScreen> {
     );
   }
 
-  Widget _buildInputField(String label, TextEditingController controller, String hintText) {
+  Widget _buildInputField(String label, String hintText) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: TextField(
-        controller: controller,
         decoration: InputDecoration(
-          labelText: label, // Etiqueta que aparecerá encima del campo
-          hintText: hintText, // El hint que aparecerá dentro del campo
+          labelText: label,
+          hintText: hintText,
           filled: true,
-          fillColor: Colors.black,
-          labelStyle: TextStyle(color: Colors.white),
-          hintStyle: TextStyle(color: Colors.grey), // Estilo para el hint (texto sugerido)
+          fillColor: Colors.black, // Fondo negro para los campos
+          labelStyle: TextStyle(color: Colors.white), // Color blanco para el texto
           border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
-            borderSide: BorderSide(color: Colors.grey),
+            borderRadius: BorderRadius.circular(8), // Bordes redondeados
+            borderSide: BorderSide(color: Colors.grey), // Borde gris claro
           ),
         ),
-        style: TextStyle(color: Colors.white),
+        style: TextStyle(color: Colors.white), // Texto en blanco dentro del campo
       ),
     );
   }
@@ -462,83 +343,28 @@ class _AddMatchScreenState extends State<AddMatchScreen> {
   Widget _buildMatchFormatField() {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: ExpansionTile(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              "Formato del partido",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-                color: Colors.teal,
-              ),
+      child: InkWell(
+        onTap: () {
+          // Acción al tocar el formato del partido
+        },
+        child: InputDecorator(
+          decoration: InputDecoration(
+            labelText: "Formato del partido",
+            filled: true,
+            fillColor: Colors.black, // Fondo negro
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: BorderSide(color: Colors.grey),
             ),
-          ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("[League]", style: TextStyle(color: Colors.teal, fontWeight: FontWeight.bold)),
+              Icon(Icons.keyboard_arrow_down, color: Colors.white),
+            ],
+          ),
         ),
-        backgroundColor: Colors.black,
-        collapsedBackgroundColor: Colors.black,
-        children: <Widget>[
-          _buildInputField("N° de jugadores", _numeroJugadoresController, "11"),
-          _buildInputField("N° de sustitutos", _numeroSustitutosController, "5"),
-          _buildDropdownField(
-            label: "Número de periodos",
-            currentValue: _selectedPeriodos,
-            items: ['1 periodo', '2 mitades', '3 tercios', '4 cuartos'],
-            onChanged: (String? newValue) {
-              setState(() {
-                _selectedPeriodos = newValue!;
-              });
-            },
-          ),
-          _buildInputField("Duración de la mitad 1", _duracionMitadController, "45"),
-          _buildInputField("Medio tiempo", _medioTiempoController, "15"),
-
-          _buildDropdownField(
-            label: "Penaltis",
-            currentValue: _selectedPenaltis,
-            items: ['No', 'Sí'],
-            onChanged: (String? newValue) {
-              setState(() {
-                _selectedPenaltis = newValue!;
-              });
-            },
-          ),
-          _buildDropdownField(
-            label: "Registrar goleadores",
-            currentValue: _selectedRegistrarGoleadores,
-            items: ['No', 'Sí'],
-            onChanged: (String? newValue) {
-              setState(() {
-                _selectedRegistrarGoleadores = newValue!;
-              });
-            },
-          ),
-          _buildDropdownField(
-            label: "Expulsiones temporales",
-            currentValue: _selectedExpulsionesTemporales,
-            items: ['Sin expulsión temporal', '5 minutos', '10 minutos'],
-            onChanged: (String? newValue) {
-              setState(() {
-                _selectedExpulsionesTemporales = newValue!;
-              });
-            },
-          ),
-          // Añadimos el Checkbox para "Recordar configuración"
-          CheckboxListTile(
-            title: Text(
-              "Recordar configuración",
-              style: TextStyle(color: Colors.white),
-            ),
-            value: _rememberConfiguration,
-            activeColor: Colors.teal,
-            onChanged: (bool? value) {
-              setState(() {
-                _rememberConfiguration = value ?? false;
-              });
-            },
-          ),
-        ],
       ),
     );
   }
@@ -546,52 +372,29 @@ class _AddMatchScreenState extends State<AddMatchScreen> {
   Widget _buildOptionalField() {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Dropdown para seleccionar el rol
-          DropdownButtonFormField<String>(
-            value: _selectedRole, // El valor predeterminado es "Árbitro"
-            decoration: InputDecoration(
-              labelText: "Rol del miembro arbitral",
-              filled: true,
-              fillColor: Colors.black,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-                borderSide: BorderSide(color: Colors.grey),
-              ),
+      child: InkWell(
+        onTap: () {
+          // Acción para mostrar más campos opcionales
+        },
+        child: InputDecorator(
+          decoration: InputDecoration(
+            labelText: "Miembros del equipo arbitral",
+            filled: true,
+            fillColor: Colors.black,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: BorderSide(color: Colors.grey),
             ),
-            dropdownColor: Colors.black,
-            items: ['Árbitro', 'Árbitro asistente', 'Cuarto árbitro', 'Asesor']
-                .map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(
-                  value,
-                  style: TextStyle(color: Colors.white),
-                ),
-              );
-            }).toList(),
-            onChanged: (String? newValue) {
-              setState(() {
-                _selectedRole = newValue ?? 'Árbitro'; // Usar "Árbitro" como valor predeterminado
-              });
-            },
           ),
-
-          SizedBox(height: 20),
-
-          // Mostrar el campo de texto solo si se selecciona un rol
-          if (_selectedRole != null && _selectedRole!.isNotEmpty)
-            _buildInputField(
-                "Nombre del $_selectedRole",
-                _roleControllers[_selectedRole] ?? TextEditingController(), // Si es null, usa un controlador por defecto
-                "Escribe el nombre del $_selectedRole"
-            ),
-        ],
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Opcional", style: TextStyle(color: Colors.grey)),
+              Icon(Icons.keyboard_arrow_down, color: Colors.white),
+            ],
+          ),
+        ),
       ),
     );
   }
-
-
 }
